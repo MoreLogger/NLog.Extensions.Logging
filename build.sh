684e315b00e6f951591b880562a11d2727dcc927
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-06-01 00:06:43 +0200

    WIP

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview1/scripts/obtain/dotnet-install.sh | bash /dev/stdin --version 1.0.0-preview1-002702 --install-dir ~/dotnet
 
 dotnet restore src/NLog.Extensions.Logging
-dotnet build --configuration release src/NLog.Extensions.Logging
+dotnet build   src/NLog.Extensions.Logging --configuration release
